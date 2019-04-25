221b516806c02eee870926323dde1b77dd21b1a9
Justin Le <justin@jle.im>
2019-04-25 14:59:17 -0700

    beginning to integrate servant-docs

---
+++ example/greet.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# OPTIONS_GHC -fno-warn-orphans  #-}
+
+import           Control.Lens
+import           Control.Monad
+import           Data.Aeson
+import           Data.Proxy
+import           Data.String.Conversions
+import           Data.Text               (Text)
+import           GHC.Generics
+import           Servant.API
+import           Servant.CLI
+import           Servant.Docs
+
+-- * Example
+
+-- | A greet message data type
+newtype Greet = Greet Text
+  deriving (Generic, Show)
+
+-- | We can get JSON support automatically. This will be used to parse
+-- and encode a Greeting as 'JSON'.
+instance FromJSON Greet
+instance ToJSON Greet
+
+-- | We can also implement 'MimeRender' for additional formats like 'PlainText'.
+instance MimeRender PlainText Greet where
+    mimeRender Proxy (Greet s) = "\"" <> cs s <> "\""
+
+-- We add some useful annotations to our captures,
+-- query parameters and request body to make the docs
+-- really helpful.
+instance ToCapture (Capture "name" Text) where
+  toCapture _ = DocCapture "name" "name of the person to greet"
+
+instance ToCapture (Capture "greetid" Text) where
+  toCapture _ = DocCapture "greetid" "identifier of the greet msg to remove"
+
+instance ToParam (QueryParam "capital" Bool) where
+  toParam _ =
+    DocQueryParam "capital"
+                  ["true", "false"]
+                  "Get the greeting message in uppercase (true) or not (false).\
+                  \Default is false."
+                  Normal
+
+instance ToSample Greet where
+  toSamples _ =
+    [ ("If you use ?capital=true", Greet "HELLO, HASKELLER")
+    , ("If you use ?capital=false", Greet "Hello, haskeller")
+    ]
+
+instance ToSample Int where
+  toSamples _ = singleSample 1729
+
+-- We define some introductory sections, these will appear at the top of the
+-- documentation.
+--
+-- We pass them in with 'docsWith', below. If you only want to add
+-- introductions, you may use 'docsWithIntros'
+intro1 :: DocIntro
+intro1 = DocIntro "On proper introductions." -- The title
+    [ "Hello there."
+    , "As documentation is usually written for humans, it's often useful \
+      \to introduce concepts with a few words." ] -- Elements are paragraphs
+
+intro2 :: DocIntro
+intro2 = DocIntro "This title is below the last"
+    [ "You'll also note that multiple intros are possible." ]
+
+
+-- API specification
+type TestApi =
+       -- GET /hello/:name?capital={true, false}  returns a Greet as JSON or PlainText
+       "hello" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON, PlainText] Greet
+
+       -- POST /greet with a Greet as JSON in the request body,
+       --             returns a Greet as JSON
+  :<|> "greet" :> ReqBody '[JSON] Greet :> Post '[JSON] (Headers '[Header "X-Example" Int] Greet)
+
+       -- DELETE /greet/:greetid
+  :<|> "greet" :> Capture "greetid" Text :> Delete '[JSON] NoContent
+
+testApi :: Proxy TestApi
+testApi = Proxy
+
+-- Build some extra information for the DELETE /greet/:greetid endpoint. We
+-- want to add documentation about a secret unicorn header and some extra
+-- notes.
+extra :: ExtraInfo TestApi
+extra =
+    extraInfo (Proxy :: Proxy ("greet" :> Capture "greetid" Text :> Delete '[JSON] NoContent)) $
+             defAction & headers <>~ ["unicorns"]
+                       & notes   <>~ [ DocNote "Title" ["This is some text"]
+                                     , DocNote "Second secton" ["And some more"]
+                                     ]
+
+main :: IO ()
+main = void $ parseClient testApi
--- src/Servant/CLI.hs
+++ src/Servant/CLI.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
@@ -15,17 +16,22 @@ module Servant.CLI (
   ) where
 
 import           Data.Bifunctor
+import           Data.Char
 import           Data.Proxy
 import           GHC.TypeLits
 import           Options.Applicative
 import           Options.Applicative.Types
 import           Servant.API
+import           Servant.API.ContentTypes
 import           Servant.API.Modifiers
 import           Servant.Client
 import           Servant.Client.Core
+import           Servant.Docs
+import           Text.Printf
+import           Type.Reflection
 import qualified Data.Text                 as T
 
-class HasClient m api => HasCLI m api where
+class (HasDocs api, HasClient m api) => HasCLI m api where
     type CLI m api
 
     clientParser_ :: Proxy m -> Proxy api -> Client m api -> Parser (m (CLI m api))
@@ -45,22 +51,30 @@ instance (KnownSymbol path, HasCLI m api) => HasCLI m (path :> api) where
       command (symbolVal (Proxy @path))
               (info (clientParser_ pm (Proxy @api) api <**> helper) mempty)
 
-instance ( KnownSymbol capture
+instance ( KnownSymbol sym
          , FromHttpApiData a
          , ToHttpApiData a
+         , Typeable a
+         , ToCapture (Capture sym a)
          , HasCLI m api
-         ) => HasCLI m (Capture' mods capture a :> api) where
-    type CLI m (Capture' mods capture a :> api) = CLI m api
+         ) => HasCLI m (Capture' mods sym a :> api) where
+    type CLI m (Capture' mods sym a :> api) = CLI m api
     clientParser_ pm _ api = BindP arg $ clientParser_ pm (Proxy @api) . api
       where
         arg :: Parser a
         arg = argument (eitherReader (first T.unpack . parseUrlPiece @a . T.pack))
-                ( metavar (symbolVal (Proxy @capture)) )
+                ( metavar (map toUpper capType)
+               <> help (printf "%s: %s (%s)" _capSymbol _capDesc capType)
+                )
+        capType = show $ typeRep @a
+        DocCapture{..} = toCapture (Proxy @(Capture sym a))
 
 instance ( KnownSymbol sym
          , FromHttpApiData a
          , ToHttpApiData a
          , SBoolI (FoldRequired' 'False mods)
+         , Typeable a
+         , ToParam (QueryParam' mods sym a)
          , HasCLI m api
          ) => HasCLI m (QueryParam' mods sym a :> api) where
     type CLI m (QueryParam' mods sym a :> api) = CLI m api
@@ -68,13 +82,21 @@ instance ( KnownSymbol sym
       where
         opt :: Parser a
         opt = option (eitherReader (first T.unpack . parseUrlPiece @a . T.pack))
-                ( long (symbolVal (Proxy @sym)) )
+                ( metavar (map toUpper pType)
+               <> long pName
+               <> help (printf "%s: %s (%s)" _paramName _paramDesc pType)
+                )
         opt' :: Parser (If (FoldRequired' 'False mods) a (Maybe a))
         opt' = case sbool @(FoldRequired' 'False mods) of
           STrue  -> opt
           SFalse -> optional opt
+        pType = show $ typeRep @a
+        pName = symbolVal (Proxy @sym)
+        DocQueryParam{..} = toParam (Proxy @(QueryParam' mods sym a))
+        -- TODO: experiment with more detailed help doc
 
 instance ( KnownSymbol sym
+         , ToParam (QueryFlag sym)
          , HasCLI m api
          ) => HasCLI m (QueryFlag sym :> api) where
     type CLI m (QueryFlag sym :> api) = CLI m api
@@ -87,6 +109,9 @@ instance ( cts' ~ (ct ': cts)
          , RunClient m
          , MimeUnrender ct a
          , ReflectMethod method
+         , ToSample a
+         , AllMimeRender cts' a
+         , KnownNat status
          ) => HasCLI m (Verb method status cts' a) where
 
     type CLI m (Verb method status cts' a) = a
